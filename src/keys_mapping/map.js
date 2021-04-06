94b519c074768738ef6a7cad6af7be6b60f7f59d
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-05 22:39:28 -0300

    feat(keys_mapping): export available key maps

---
+++ src/keys_mapping/map.js
@@ -0,0 +1,7 @@
+import { KeyNames as PanasonicKeyNames } from './devices/panasonic'
+import { KeyNames as BrowserKeyNames } from './devices/browser'
+
+export const KeyMap = {
+  browser: BrowserKeyNames,
+  panasonic: PanasonicKeyNames,
+}
