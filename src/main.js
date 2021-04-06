87a93eec82aa1e38091261d9a9df2dfc0c18d517
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-05 22:37:55 -0300

    refactor(main): export key names constants

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import TVsKeyMappingPlugin from './key_watcher/key_watcher'
+import { KeyNames } from './keys_mapping/key_name'
 
-export { TVsKeyMappingPlugin as Watcher }
+export {
+  TVsKeyMappingPlugin as Watcher,
+  KeyNames,
+}
