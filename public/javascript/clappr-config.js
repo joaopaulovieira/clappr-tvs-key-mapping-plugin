217c0f6cf15a6c8fbb1086c07f48f149a9a27898
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-05 23:30:47 -0300

    feat(public): add example of use

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -1,10 +1,18 @@
+Clappr.Log.setLevel(Clappr.Log.LEVEL_INFO)
+
+const onReadyCallback = function() {
+  this.core.listenTo(this.core, Clappr.Events.Custom.CORE_SMART_TV_KEY_PRESSED, keyName => Clappr.Log.info('keyName', keyName))
+}
+
 const player = new Clappr.Player({
   height: '100%',
   width: '100%',
   source: 'http://clappr.io/highline.mp4',
   poster: 'http://clappr.io/poster.png',
   playback: { controls: true },
+  tvsKeyMapping: { deviceToMap: 'browser' },
   plugins: [window.TVsKeyMappingPlugin.Watcher],
+  events: { onReady: onReadyCallback },
 })
 
 player.attachTo(document.body)
