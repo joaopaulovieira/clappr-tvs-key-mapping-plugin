6b935de743f68f155426eef649bbbe9fb97bf6c5
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-11 14:41:01 -0300

    feat(keys_mapping): map samsung tizen devices

--- src/keys_mapping/map.js
+++ src/keys_mapping/map.js
@@ -1,7 +1,10 @@
-import { KeyNames as PanasonicKeyNames } from './devices/panasonic'
+/* eslint-disable camelcase */
 import { KeyNames as BrowserKeyNames } from './devices/browser'
+import { KeyNames as TizenKeyNames } from './devices/samsung_tizen'
+import { KeyNames as PanasonicKeyNames } from './devices/panasonic'
 
 export const KeyMap = {
   browser: BrowserKeyNames,
   panasonic: PanasonicKeyNames,
+  samsung_tizen: TizenKeyNames,
 }
