fe0164353f8a4d09da99fdb44c162730be1527d4
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-11 17:51:11 -0300

    feat(keys_mapping): map lg webos devices

--- src/keys_mapping/map.js
+++ src/keys_mapping/map.js
@@ -2,11 +2,13 @@
 import { KeyNames as BrowserKeyNames } from './devices/browser'
 import { KeyNames as TizenKeyNames } from './devices/samsung_tizen'
 import { KeyNames as OrsayKeyNames } from './devices/samsung_orsay'
+import { KeyNames as WebOSKeyNames } from './devices/lg_webos'
 import { KeyNames as PanasonicKeyNames } from './devices/panasonic'
 
 export const KeyMap = {
   browser: BrowserKeyNames,
-  panasonic: PanasonicKeyNames,
   samsung_tizen: TizenKeyNames,
   samsung_orsay: OrsayKeyNames,
+  lg_webos: WebOSKeyNames,
+  panasonic: PanasonicKeyNames,
 }
