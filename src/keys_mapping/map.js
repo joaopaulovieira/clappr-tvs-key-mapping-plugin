e1a282a9a58ba0ba519f52924f506fc96289b25d
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-11 17:18:18 -0300

    feat(keys_mapping): map samsung orsay devices

--- src/keys_mapping/map.js
+++ src/keys_mapping/map.js
@@ -1,10 +1,12 @@
 /* eslint-disable camelcase */
 import { KeyNames as BrowserKeyNames } from './devices/browser'
 import { KeyNames as TizenKeyNames } from './devices/samsung_tizen'
+import { KeyNames as OrsayKeyNames } from './devices/samsung_orsay'
 import { KeyNames as PanasonicKeyNames } from './devices/panasonic'
 
 export const KeyMap = {
   browser: BrowserKeyNames,
   panasonic: PanasonicKeyNames,
   samsung_tizen: TizenKeyNames,
+  samsung_orsay: OrsayKeyNames,
 }
