bb846b55d262a72311b47907b0b4b045c0097789
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-04-21 02:23:32 -0300

    refactor(public): update demo page lighthouse score

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -1,18 +1,18 @@
+/* eslint-disable */
 Clappr.Log.setLevel(Clappr.Log.LEVEL_INFO)
 
 const onReadyCallback = function() {
   this.core.listenTo(this.core, Clappr.Events.Custom.CORE_SMART_TV_KEY_PRESSED, keyName => Clappr.Log.info('keyName', keyName))
 }
 
-const player = new Clappr.Player({
-  height: '100%',
-  width: '100%',
+var player = new Clappr.Player({
   source: 'http://clappr.io/highline.mp4',
-  poster: 'http://clappr.io/poster.png',
-  playback: { controls: true },
+  height: '320px',
+  width: '640px',
   tvsKeyMapping: { deviceToMap: 'browser' },
+  playback: { controls: true },
   plugins: [window.TVsKeyMappingPlugin.Watcher],
   events: { onReady: onReadyCallback },
-})
+});
 
-player.attachTo(document.body)
+player.attachTo(document.body);
